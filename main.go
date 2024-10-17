ecb8cb8bd5726ac70a5975258633cbc580f0c767
Sergio Chamba <sergiochamba@outlook.com>
2024-10-16 18:18:26 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "flag"
+    "os"
+
+    "k8s.io/apimachinery/pkg/runtime"
+    utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+    clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+    ctrl "sigs.k8s.io/controller-runtime"
+    "sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+    "github.com/sergiochamba/nginx-lb-operator/controllers"
+    // +kubebuilder:scaffold:imports
+)
+
+var (
+    scheme   = runtime.NewScheme()
+    setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+    utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+    // +kubebuilder:scaffold:scheme
+}
+
+func main() {
+    var metricsAddr string
+    var enableLeaderElection bool
+    flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+    flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+        "Enable leader election for controller manager.")
+
+    opts := zap.Options{
+        Development: true,
+    }
+    opts.BindFlags(flag.CommandLine)
+    flag.Parse()
+
+    ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+    mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+        Scheme:             scheme,
+        MetricsBindAddress: metricsAddr,
+        LeaderElection:     enableLeaderElection,
+        LeaderElectionID:   "nginx-lb-operator-lock",
+    })
+    if err != nil {
+        setupLog.Error(err, "unable to start manager")
+        os.Exit(1)
+    }
+
+    if err = (&controllers.ServiceReconciler{
+        Client: mgr.GetClient(),
+        Scheme: mgr.GetScheme(),
+    }).SetupWithManager(mgr); err != nil {
+        setupLog.Error(err, "unable to create controller", "controller", "Service")
+        os.Exit(1)
+    }
+
+    // +kubebuilder:scaffold:builder
+
+    setupLog.Info("starting manager")
+    if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+        setupLog.Error(err, "problem running manager")
+        os.Exit(1)
+    }
+}
